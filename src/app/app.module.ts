2b9c4b4d161e9480b2f56a95cadd14b05167d70f
Facundo Comercio <facundocomercio@outlook.com>
2022-12-06 00:10:47 -0300

    trabajando boton task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadersComponent } from './components/headers/headers.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeadersComponent
+    HeadersComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
