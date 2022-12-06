a54341a130573d7bbc035bc3af086009921f9eca
Facundo Comercio <facundocomercio@outlook.com>
2022-12-06 10:31:44 -0300

    creacion de mocks de tasks

---
+++ src/app/mock-servic.ts
@@ -0,0 +1,10 @@
+import { task } from "./task";
+
+export const tasks: task[]= [
+    {
+        id:1,
+        text:"Terminar modulo de Angular",
+        dia: "Agosto 5 a las 12",
+        recordatorio: true
+    }
+]
